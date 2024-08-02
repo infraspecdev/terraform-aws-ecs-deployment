23b8c7f9ba21a49a37a205c76cfb60db461c71af
saumitra-dev <saumitra@infraspec.dev>
2024-08-02 10:48:39 +0530

    chore: re-order ecs service and task definition variables

--- variables.tf
+++ variables.tf
@@ -9,20 +9,20 @@ variable "vpc_id" {
 }
 
 ################################################################################
-# ECS Task Definition
-################################################################################
-
-variable "task_definition" {
-  description = "ECS Task Definition to use for running tasks."
-  type        = any
-}
-
-################################################################################
 # ECS Service
 ################################################################################
 
 variable "service" {
   description = "Configuration for ECS Service."
+  type        = any
+}
+
+################################################################################
+# ECS Task Definition
+################################################################################
+
+variable "task_definition" {
+  description = "ECS Task Definition to use for running tasks."
   type        = any
 }
 
